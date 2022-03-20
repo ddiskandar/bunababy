10c27c0c09dde1fa003f6fdcc866eac7b0db827a
Dede Iskandar <dd@smkplusalfarhan.sch.id>
2022-03-20 14:29:23 +0700

    wip

--- app/Http/Livewire/Admin/EditMidwife.php
+++ app/Http/Livewire/Admin/EditMidwife.php
@@ -8,9 +8,13 @@ use Livewire\Component;
 class EditMidwife extends Component
 {
     public $midwife;
-    public $kecamatan_id;
+    public $kecamatan_id = '';
 
     public $state = [];
+
+    public $rules = [
+        'kecamatan_id' => 'required|exists:kecamatans,id'
+    ];
 
     public function mount(User $user)
     {
@@ -18,24 +22,36 @@ class EditMidwife extends Component
         $this->state = $user->toArray();
     }
 
+    protected $listeners = ['refreshPage' => '$refresh'];
+
     public function addWilayah()
     {
+
+        $this->validate();
         $this->midwife->kecamatans()->attach([$this->kecamatan_id]);
-        $this->render();
+        $this->kecamatan_id = '';
+        $this->emit('refreshPage');
     }
 
     public function deleteWilayah($id)
     {
         $this->midwife->kecamatans()->detach([$id]);
+        $this->emit('refreshPage');
     }
 
     public function render()
     {
         $kecamatans = $this->midwife->kecamatans;
 
+        $kecamatansFiltered = \DB::table('kecamatans')
+            ->whereNotIn('id', $this->midwife->kecamatans
+            ->pluck('id'))
+            ->orderBy('name')
+            ->get();
+
         return view('livewire.admin.edit-midwife', [
-            'kecamatans' => $kecamatans
-        ])
-        ->layout('layouts.app');
+            'kecamatans' => $kecamatans,
+            'kecamatansFiltered' => $kecamatansFiltered
+        ])->layout('layouts.app');
     }
 }
--- resources/views/livewire/admin/edit-midwife.blade.php
+++ resources/views/livewire/admin/edit-midwife.blade.php
@@ -67,26 +67,26 @@
         <div class="w-full p-5 lg:p-6 grow">
             <div class="space-y-1">
                 <x-label class="" >Wilayah</x-label>
-                <div class="flex gap-2 flex-wrap py-2">
+                <div class="flex flex-wrap gap-2 py-2">
                     @foreach ($kecamatans as $kecamatan)
-                        <div class="font-semibold inline-flex px-4 py-1 leading-4 items-center space-x-1 text-xs rounded-full text-bunababy-200 bg-bunababy-50">
+                        <div class="inline-flex items-center px-4 py-1 space-x-1 text-xs font-semibold leading-4 rounded-full text-bunababy-200 bg-bunababy-50">
                             <span>{{ $kecamatan->name }}</span>
                             <button
                                 wire:click="deleteWilayah({{ $kecamatan->id }})"
                                 type="button"
-                                class="focus:outline-none text-pink-600 hover:text-pink-400 focus:ring focus:ring-pink-500 focus:ring-opacity-50 active:text-pink-600">
-                            <svg class="hi-solid hi-x inline-block w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"/></svg>
+                                class="text-pink-600 focus:outline-none hover:text-pink-400 focus:ring focus:ring-pink-500 focus:ring-opacity-50 active:text-pink-600">
+                            <svg class="inline-block w-4 h-4 hi-solid hi-x" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"/></svg>
                             </button>
                         </div>
                     @endforeach
                 </div>
             </div>
 
-            <div class="space-y-1 mt-4">
+            <div class="mt-4 space-y-1">
                 <x-label class="" for="kecamatan_id">Tambah Wilayah baru</x-label>
                 <select wire:model="kecamatan_id" class="w-full rounded-md border-bunababy-50 focus:border-bunababy-100 focus:ring-0 focus:ring-bunababy-100 focus:ring-opacity-50 disabled:bg-slate-100 disabled:opacity-75" type="text" id="kecamatan_id">
                     <option value="" selected>-- Pilih salah satu</option>
-                    @foreach (DB::table('kecamatans')->whereNotIn('id', $midwife->kecamatans->pluck('id'))->orderBy('name')->get() as $kecamatan)
+                    @foreach ($kecamatansFiltered as $kecamatan)
                         <option value="{{ $kecamatan->id }}">{{ $kecamatan->name }}</option>
                     @endforeach
                 </select>
