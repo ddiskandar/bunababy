f1a72612642d5592ffd99adfe0a4d9434e7bb067
ddiskandar <dd@smkplusalfarhan.sch.id>
2022-05-18 07:24:20 +0700

    Update OptionSeeder.php

--- database/seeders/OptionSeeder.php
+++ database/seeders/OptionSeeder.php
@@ -18,7 +18,7 @@ class OptionSeeder extends Seeder
             'site_name' => 'Bunababy Care',
             'site_location' => 'Komplek Nata Endah Blok N No. 170, Cibabat, Cimahi',
             'site_desc' => 'Mom and Baby Care',
-            'ig' => '@bunababy.care',
+            'ig' => '@bunababy_care',
             'phone' => '08997897991',
             'account' => 'BCA 2810417067',
             'account_name' => 'a/n Febrianti Nur Azizah',
