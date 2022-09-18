c8d6a9df5d864c58b81fdccbf5845960008a1a1b
Dede Iskandar <d3215k@gmail.com>
2022-09-19 04:20:33 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,10 @@ class HomeController extends Controller
 {
     public function show()
     {
+        if(auth()->check() && ! auth()->user()->isClient()) {
+            return redirect()->route('dashboard');
+        }
+
         $hasAddress = isset(auth()->user()->address);
         $hasPhone = isset(auth()->user()->profile->phone);
         $profileCompleted = ($hasPhone AND $hasAddress);
