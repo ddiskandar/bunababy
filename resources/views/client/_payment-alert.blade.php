64044d0ffa730259cab763c1bb0aff5382d7f6b3
Dede Iskandar <d3215k@gmail.com>
2022-09-18 17:06:29 +0700

    Update _payment-alert.blade.php

--- resources/views/client/_payment-alert.blade.php
+++ resources/views/client/_payment-alert.blade.php
@@ -9,8 +9,8 @@
             x-transition:leave-start="opacity-100 transform translate-x-0"
             x-transition:leave-end="opacity-0 transform translate-x-8"
             style="display: none !important"
-            class="sticky inset-x-0 p-3 bg-red-600 shadow-lg top-[67px] z-60 sm:py-5 sm:px-8">
-            <div class="flex items-center justify-between container mx-auto sm:px-12">
+            class="sticky inset-x-0 p-3 bg-red-600 shadow-lg top-[67px] z-60 px-6">
+            <div class="flex items-center justify-between">
                 <div class="inline-flex items-center text-white">
                     <svg class="inline-block w-8 h-8 flex-none opacity-50" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4.9522 16.3536L10.2152 5.85658C10.9531 4.38481 13.0539 4.3852 13.7913 5.85723L19.0495 16.3543C19.7156 17.6841 18.7487 19.25 17.2613 19.25H6.74007C5.25234 19.25 4.2854 17.6835 4.9522 16.3536Z"></path>
