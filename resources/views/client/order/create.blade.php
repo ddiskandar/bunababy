b39db64ba42e4323da7632e5adc44686f86e488a
Dede Iskandar <d3215k@gmail.com>
2022-09-18 11:41:54 +0700

    wip

--- resources/views/client/order/create.blade.php
+++ resources/views/client/order/create.blade.php
@@ -2,33 +2,28 @@
 
     @include('layouts._order-step')
 
-<div>
-    <div class="space-y-4">
-
-        <div class="px-6 py-4 my-2 bg-white">
-            <div>
-                <x-title>Pilih Tempat</x-title>
-                @livewire('order.select-place')
-            </div>
+<div class="space-y-2">
+    <div class="px-6 py-4 bg-white">
+        <div>
+            <x-title>Pilih Tempat</x-title>
+            @livewire('order.select-place')
         </div>
+    </div>
 
-        <div class="px-6 py-4 my-2 bg-white">
-            <div>
-                @livewire('select-location')
-            </div>
-        </div>
+    <div class="px-6 py-4 bg-white">
+        @livewire('select-location')
+    </div>
 
-        <div class="px-6 py-4 my-2 bg-white">
-            @if (session('order.place') == 1)
-            <div class="flex-1 mt-6 md:mt-0">
-                @livewire('order.list-midwife')
-            </div>
-            @else
-            <div class="flex-1 mt-6 md:mt-0">
-                @livewire('order.clinic')
-            </div>
-            @endif
+    <div class="px-6 py-4 bg-white">
+        @if (session('order.place') == 1)
+        <div class="flex-1 mt-6 md:mt-0">
+            @livewire('order.list-midwife')
         </div>
+        @else
+        <div class="flex-1 mt-6 md:mt-0">
+            @livewire('order.clinic')
+        </div>
+        @endif
     </div>
 </div>
 
