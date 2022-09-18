e9236008aee26b3fd2b3f8cc797f39cae7f9d373
Dede Iskandar <d3215k@gmail.com>
2022-09-18 20:35:14 +0700

    Update cart.blade.php

--- resources/views/client/order/cart.blade.php
+++ resources/views/client/order/cart.blade.php
@@ -4,14 +4,14 @@
 
     @include('layouts._order-step')
 
-    <div class="space-y-2 mt-2">
+    <div class="mt-2 space-y-2">
         @if (! auth()->check() && session()->missing('order.status'))
             <div class="px-6 py-4 bg-white">
                 <h4 class="mb-1 font-semibold">
                     Khusus Pelanggan
                 </h4>
                 <p class="mb-5">
-                    Bila sebelumnya sudah pernah pesan treatment, silahkan untuk login. Lupa password hubungi <a href="https://api.whatsapp.com/send?phone={{ to_wa_indo(\DB::table('options')->select('phone')->first()->phone) }}&text=Halo+Bunababy_Care.+Perkenalkan+saya+dengan+" target="_blank" class="text-bunababy-200 font-semibold">Admin</a>.
+                    Bila sebelumnya sudah pernah pesan treatment, silahkan untuk login.
                 </p>
                 <a href="{{ route('login') }}">
                     <x-button>
@@ -20,7 +20,7 @@
                 </a>
 
             </div>
-            <div class="px-6 py-12 bg-green-600 text-white">
+            <div class="px-6 py-12 text-white bg-green-600">
                 <a href="{{ route('order.check') }}" class="inline-flex items-center justify-center py-2 leading-5 border border-transparent rounded " >
                     Klik disini bila sekarang treatment pertama
                 </a>
@@ -43,7 +43,7 @@
             @livewire('order.select-treatments')
         </div>
 
-        <div class="px-6 py-4 bg-bunababy-100 text-white">
+        <div class="px-6 py-4 text-white bg-bunababy-100">
             @livewire('order.cart-summary')
         </div>
 
