3d51e4af3662317606f3d2ffcff62873af292bf7
Dede Iskandar <d3215k@gmail.com>
2022-09-18 18:52:16 +0700

    wip

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,8 +17,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('order:payment')->everyMinute();
-        $schedule->command('backup:clean')->daily()->at('01:00');
-        $schedule->command('backup:run')->daily()->at('01:30');
+        // $schedule->command('backup:clean')->daily()->at('01:00');
+        // $schedule->command('backup:run')->daily()->at('01:30');
     }
 
     /**
