45b70b98ea38c847e53182512156b82b48b2bb22
Dede Iskandar <d3215k@gmail.com>
2022-09-19 11:12:35 +0700

    Update daily-midwife-schedules.blade.php

--- resources/views/daily-midwife-schedules.blade.php
+++ resources/views/daily-midwife-schedules.blade.php
@@ -44,7 +44,7 @@
             </a> --}}
 
             <div class="w-full text-sm bg-white px-6 py-3 rounded ">
-                <div class="flex flex-col gap-3 mb-4 md:flex-row md:justify-between md:items-center">
+                <div class="flex flex-col gap-1 mb-4 md:flex-row md:justify-between md:items-center">
                     <div class="flex justify-between items-start">
                         <div class="text-lg font-semibold hover:underline">
                             <a href="{{ route('orders.show', $schedule->id) }}">
@@ -62,6 +62,16 @@
                             {{ $schedule->status() }}
                         </div>
                     </div>
+                    <div class="flex items-center gap-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <circle cx="12" cy="12" r="9"></circle>
+                            <polyline points="12 7 12 12 15 15"></polyline>
+                         </svg>
+                        <div class="text-sm font-medium">
+                            <div>{{ $schedule->start_datetime->isoFormat('HH:mm') . ' - ' . $schedule->end_datetime->isoFormat('HH:mm') }} WIB</div>
+                        </div>
+                    </div>
                     <div>
                         <div>
                             {{ $schedule->place() }}
@@ -73,19 +83,6 @@
                             {{ $schedule->address->kecamatan->name }}
                         </div>
                     </div>
-                    <div class="flex items-center gap-2">
-                        <svg class="w-8 h-8 text-slate-500 flex-shrink-0" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4.75 8.75C4.75 7.64543 5.64543 6.75 6.75 6.75H17.25C18.3546 6.75 19.25 7.64543 19.25 8.75V17.25C19.25 18.3546 18.3546 19.25 17.25 19.25H6.75C5.64543 19.25 4.75 18.3546 4.75 17.25V8.75Z"></path>
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M8 4.75V8.25"></path>
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M16 4.75V8.25"></path>
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M7.75 10.75H16.25"></path>
-                        </svg>
-                        <div class="text-sm font-medium">
-                            <div>{{ $schedule->start_datetime->isoFormat('dddd, DD MMMM Y') }}</div>
-                            <div>{{ $schedule->start_datetime->isoFormat('HH:mm') . ' - ' . $schedule->end_datetime->isoFormat('HH:mm') }} WIB</div>
-                        </div>
-                    </div>
-
                 </div>
             </div>
 
