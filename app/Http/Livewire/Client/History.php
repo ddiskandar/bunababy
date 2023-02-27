62df28483d130050741c40cd7e9c0ca544b620de
Dede Iskandar <d3215k@gmail.com>
2023-02-27 10:10:32 +0700

    wip

--- app/Http/Livewire/Client/History.php
+++ app/Http/Livewire/Client/History.php
@@ -13,11 +13,14 @@ class History extends Component
     {
         $reservations = Order::query()
             ->where('client_user_id', auth()->id())
-            ->where('status', 'like', '%' . $this->filterStatus . '%' )
+            ->where('status', 'like', '%' . $this->filterStatus . '%')
             ->get();
+
+        $hasActiveReservation = $reservations->where('status', Order::STATUS_LOCKED)->count() > 0;
 
         return view('client.history.history', [
             'reservations' => $reservations,
+            'hasActiveReservation' => $hasActiveReservation,
         ]);
     }
 }
--- resources/views/client/history/history.blade.php
+++ resources/views/client/history/history.blade.php
@@ -34,19 +34,24 @@
         </div>
     </div>
 
-    <div class="fixed max-w-screen-sm py-3 right-4 bottom-24 z-90">
-        <a href="{{ route('order.create') }}"
-            class="flex flex-col items-center p-2 border-2 rounded-full cursor-pointer bg-bunababy-200 border-bunababy-200 text-bunababy-200 hover:text-bunababy-200"
-        >
-            <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-white" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <rect x="4" y="5" width="16" height="16" rx="2"></rect>
-                <line x1="16" y1="3" x2="16" y2="7"></line>
-                <line x1="8" y1="3" x2="8" y2="7"></line>
-                <line x1="4" y1="11" x2="20" y2="11"></line>
-                <line x1="10" y1="16" x2="14" y2="16"></line>
-                <line x1="12" y1="14" x2="12" y2="18"></line>
-            </svg>
-        </a>
+    @if (!$hasActiveReservation)
+    <div class="fixed inset-x-0 bottom-24 z-90">
+        <div class="flex items-center justify-center">
+            <a href="{{ route('order.create') }}"
+                class="flex items-center px-6 py-2 border-white rounded-full shadow-lg cursor-pointer shadow-bunababy-200/25 border-3 bg-bunababy-200 text-bunababy-200 hover:text-bunababy-200"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-white" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <rect x="4" y="5" width="16" height="16" rx="2"></rect>
+                    <line x1="16" y1="3" x2="16" y2="7"></line>
+                    <line x1="8" y1="3" x2="8" y2="7"></line>
+                    <line x1="4" y1="11" x2="20" y2="11"></line>
+                    <line x1="10" y1="16" x2="14" y2="16"></line>
+                    <line x1="12" y1="14" x2="12" y2="18"></line>
+                </svg>
+                <span class="ml-2 text-xs font-semibold text-white">Buat Reservasi Baru</span>
+            </a>
+        </div>
     </div>
+    @endif
 </div>
