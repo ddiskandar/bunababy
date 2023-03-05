4e189b7f36682ee70b688c01b9f081b00530cf85
Dede Iskandar <d3215k@gmail.com>
2023-03-05 17:45:30 +0700

    wip

--- resources/views/client/order/_detail.blade.php
+++ resources/views/client/order/_detail.blade.php
@@ -2,10 +2,44 @@
 <div class="mt-2">
     @if (!$hasPayments)
         <div class="text-sm">Besar Pembayaran DP (50%)</div>
-        <div class="mb-4 text-3xl font-semibold text-red-600">{{ rupiah($order->getDpAmount()) }}</div>
+        <div class="flex items-center justify-between mb-4 ">
+            <div class="text-3xl font-semibold text-red-600">{{ rupiah($order->getDpAmount()) }}</div>
+            <div class="flex items-center gap-2 cursor-pointer text-bunababy-200" x-data x-on:click="
+                window.navigator.clipboard.writeText({{ $order->getDpAmount() }});
+                new Notification()
+                    .success()
+                    .title('Copied')
+                    .duration(3000)
+                    .send();
+                ">
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M8 8m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z"></path>
+                    <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2"></path>
+                </svg>
+                <span>Salin</span>
+            </div>
+        </div>
 
         <div class="text-sm">Total Keseluruhan Yang Harus Dibayar</div>
-        <div class="mb-4 text-3xl font-semibold text-red-600">{{ rupiah($order->getGrandTotal()) }}</div>
+        <div class="flex items-center justify-between mb-4 ">
+            <div class="text-3xl font-semibold text-red-600">{{ rupiah($order->getGrandTotal()) }}</div>
+            <div class="flex items-center gap-2 cursor-pointer text-bunababy-200" x-data x-on:click="
+                window.navigator.clipboard.writeText({{ $order->getGrandTotal() }});
+                new Notification()
+                    .success()
+                    .title('Copied')
+                    .duration(3000)
+                    .send();
+                ">
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M8 8m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z"></path>
+                    <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2"></path>
+                </svg>
+                <span>Salin</span>
+            </div>
+        </div>
 
         <div class="text-sm">Silahkan untuk segera melakukan pembayaran di ATM atau Internet Banking sebelum</div>
         <div class="mt-1 mb-4 font-semibold">{{ $order->created_at->addMinutes($options->timeout)->isoFormat('dddd, D MMMM G HH:mm') }} WIB</div>
@@ -27,11 +61,11 @@
 
     <div class="mb-4 ">
         <div class="text-sm">Mohon tuliskan berita / referensi :</div>
-        <div class="flex items-center space-x-2 font-semibold">
-            <div>
+        <div class="flex items-center justify-between mb-4 ">
+            <div class="font-semibold">
                 {{ $order->no_reg }}
             </div>
-            <div class="cursor-pointer" x-data x-on:click="
+            <div class="flex items-center gap-2 cursor-pointer text-bunababy-200" x-data x-on:click="
                 window.navigator.clipboard.writeText({{ $order->no_reg }});
                 new Notification()
                     .success()
@@ -39,11 +73,12 @@
                     .duration(3000)
                     .send();
                 ">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-bunababy-200" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                     <path d="M8 8m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z"></path>
                     <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2"></path>
-                    </svg>
+                </svg>
+                <span>Salin</span>
             </div>
         </div>
         <div class="text-sm">pada kolom berita transfer.</div>
--- resources/views/client/order/show.blade.php
+++ resources/views/client/order/show.blade.php
@@ -20,20 +20,6 @@
                         <div class="text-sm font-semibold leading-loose text-bunababy-400">ID Transaksi</div>
                         <div class="flex space-x-2 ">
                             <div class="font-semibold">{{ $order->no_reg }}</div>
-                            <div class="cursor-pointer" x-data x-on:click="
-                                window.navigator.clipboard.writeText({{ $order->no_reg }});
-                                new Notification()
-                                    .success()
-                                    .title('Copied')
-                                    .duration(3000)
-                                    .send();
-                                ">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-bunababy-200" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <path d="M8 8m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z"></path>
-                                    <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2"></path>
-                                 </svg>
-                            </div>
                         </div>
                     </div>
                     <div>
