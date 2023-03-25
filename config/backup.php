1a29179584e3ae67618e2d3d5790db8926ce2000
Dede Iskandar <d3215k@gmail.com>
2023-03-25 16:21:19 +0700

    wip

--- config/backup.php
+++ config/backup.php
@@ -204,7 +204,7 @@ return [
      */
     'monitor_backups' => [
         [
-            'name' => env('APP_NAME', 'laravel-backup'),
+            'name' => 'bunababycare-backup',
             'disks' => ['local'],
             'health_checks' => [
                 \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class => 1,
