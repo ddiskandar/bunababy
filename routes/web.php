a77c171b554d5862b075acbd3285124798bbf8f0
Dede Iskandar <d3215k@gmail.com>
2023-05-27 13:43:35 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -31,11 +31,18 @@ Route::middleware(['auth'])->group(function () {
     // Dashboard
     Route::get('/dashboard', App\Http\Controllers\DashboardController::class)->name('dashboard');
 
-    Route::middleware(['midwife'])->group(function () {
-        Route::get('/orders/{order}', [App\Http\Controllers\OrdersController::class, 'show'])->name('orders.show');
-        Route::get('/orders', [App\Http\Controllers\OrdersController::class, 'index'])->name('orders');
-        Route::get('/timetables', [App\Http\Controllers\TimetableController::class, 'index'])->name('timetables');
-        Route::get('/user/profile', App\Http\Controllers\UserProfileController::class)->name('user.profile');
+    // Owner..
+    Route::middleware(['owner'])->group(function () {
+        Route::get('/midwives/create', [App\Http\Controllers\MidwivesController::class, 'create'])->name('midwives.create');
+        Route::get('/midwives/{midwife}/edit', [App\Http\Controllers\MidwivesController::class, 'edit'])->name('midwives.edit');
+        Route::get('/midwives', [App\Http\Controllers\MidwivesController::class, 'index'])->name('midwives');
+        Route::get('/treatments', App\Http\Controllers\TreatmentsController::class)->name('treatments');
+        Route::get('/places', [App\Http\Controllers\PlaceController::class, 'index'])->name('places');
+        Route::get('/places/{place}/edit', [App\Http\Controllers\PlaceController::class, 'edit'])->name('places.edit');
+        Route::get('/treatments/categories', App\Http\Controllers\TreatmentCategoriesController::class)->name('categories');
+        Route::get('/wilayah', App\Http\Controllers\KecamatanController::class)->name('wilayah');
+        Route::get('/wilayah/kabupaten', App\Http\Controllers\KabupatenController::class)->name('kabupaten');
+        Route::get('/settings', App\Http\Controllers\SettingController::class)->name('settings');
     });
 
     // Admin...
@@ -51,18 +58,11 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/clients', [App\Http\Controllers\ClientsController::class, 'index'])->name('clients');
     });
 
-    // Owner..
-    Route::middleware(['owner'])->group(function () {
-        Route::get('/midwives/create', [App\Http\Controllers\MidwivesController::class, 'create'])->name('midwives.create');
-        Route::get('/midwives/{midwife}/edit', [App\Http\Controllers\MidwivesController::class, 'edit'])->name('midwives.edit');
-        Route::get('/midwives', [App\Http\Controllers\MidwivesController::class, 'index'])->name('midwives');
-        Route::get('/treatments', App\Http\Controllers\TreatmentsController::class)->name('treatments');
-        Route::get('/places', [App\Http\Controllers\PlaceController::class, 'index'])->name('places');
-        Route::get('/places/{place}/edit', [App\Http\Controllers\PlaceController::class, 'edit'])->name('places.edit');
-        Route::get('/treatments/categories', App\Http\Controllers\TreatmentCategoriesController::class)->name('categories');
-        Route::get('/wilayah', App\Http\Controllers\KecamatanController::class)->name('wilayah');
-        Route::get('/wilayah/kabupaten', App\Http\Controllers\KabupatenController::class)->name('kabupaten');
-        Route::get('/settings', App\Http\Controllers\SettingController::class)->name('settings');
+    Route::middleware(['midwife'])->group(function () {
+        Route::get('/orders/{order}', [App\Http\Controllers\OrdersController::class, 'show'])->name('orders.show');
+        Route::get('/orders', [App\Http\Controllers\OrdersController::class, 'index'])->name('orders');
+        Route::get('/timetables', [App\Http\Controllers\TimetableController::class, 'index'])->name('timetables');
+        Route::get('/user/profile', App\Http\Controllers\UserProfileController::class)->name('user.profile');
     });
 });
 
