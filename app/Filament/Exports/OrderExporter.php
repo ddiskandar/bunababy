a8f63af2935f1273b7cd9223642fc50262f15e8e
Dede Iskandar <d3215k@gmail.com>
2024-11-05 12:21:55 +0700

    Update OrderExporter.php

--- app/Filament/Exports/OrderExporter.php
+++ app/Filament/Exports/OrderExporter.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Exports;
 
 use App\Models\Order;
+use App\Models\Timetable;
 use Filament\Actions\Exports\ExportColumn;
 use Filament\Actions\Exports\Exporter;
 use Filament\Actions\Exports\Models\Export;
@@ -16,21 +17,32 @@ class OrderExporter extends Exporter
         return [
             ExportColumn::make('id')
                 ->label('ID'),
-            ExportColumn::make('place.name'),
-            ExportColumn::make('room.name'),
-            ExportColumn::make('customer.name'),
-            ExportColumn::make('midwife.name'),
-            ExportColumn::make('total_price'),
-            ExportColumn::make('total_duration'),
-            ExportColumn::make('total_transport'),
-            ExportColumn::make('adjustment_amount'),
-            ExportColumn::make('adjustment_name'),
-            ExportColumn::make('date'),
+            ExportColumn::make('date')
+                ->formatStateUsing(fn (Order $record) => $record->date->format('d/m/Y')),
             ExportColumn::make('start_time'),
             ExportColumn::make('end_time'),
-            ExportColumn::make('screening'),
-            // ExportColumn::make('status'),
+            ExportColumn::make('customer.name'),
+            ExportColumn::make('midwife.name'),
+            ExportColumn::make('place.name'),
+            ExportColumn::make('listTreatments')
+                ->label('Treatment'),
+            ExportColumn::make('price')
+                ->label('Harga Treatment'),
+            ExportColumn::make('transport'),
+            ExportColumn::make('adjustment_amount')
+                ->label('Adjustment'),
+            ExportColumn::make('total')
+                ->state(fn (Order $record) => $record->getGrandTotal()),
+            ExportColumn::make('status')
+                ->state(fn (Order $record) => isset($record->status) ? $record->status->getLabel() : '-' ),
             ExportColumn::make('finished_at'),
+            ExportColumn::make('keterangan')
+                ->state(function (Order $record) {
+                    return Timetable::query()
+                        ->where('midwife_id', $record->midwife_id)
+                        ->where('date', $record->date)
+                        ->first()?->status ?? '-';
+                }),
         ];
     }
 
